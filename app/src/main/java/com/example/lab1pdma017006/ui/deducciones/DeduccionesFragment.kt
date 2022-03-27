7dab2231fb48ca2b09ff554520c237638373d97c
adolfo199 <edgareeel@gmail.com>
2022-03-26 22:45:21 -0600

    segudno fragment

--- app/src/main/java/com/example/lab1pdma017006/ui/deducciones/DeduccionesFragment.kt
+++ app/src/main/java/com/example/lab1pdma017006/ui/deducciones/DeduccionesFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.lab1pdma017006.R
 
 class DeduccionesFragment : Fragment() {
@@ -20,13 +24,56 @@ class DeduccionesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.deducciones_fragment, container, false)
+        val view = inflater.inflate(R.layout.deducciones_fragment, container, false)
+        val dinero: EditText = view.findViewById(R.id.dinero)
+        val btn: Button = view.findViewById(R.id.dudeuc_btn)
+        val rentaTex:TextView = view.findViewById(R.id.textView4)
+        val issText:TextView = view.findViewById(R.id.textView5)
+        val afpText:TextView = view.findViewById(R.id.textView6)
+        val seguroText:TextView = view.findViewById(R.id.textView7)
+
+
+        btn.setOnClickListener() {
+
+            var money = dinero.text.toString().toDoubleOrNull()
+            if(money==null){
+                Toast.makeText(activity,"Introduce el salario",Toast.LENGTH_LONG)
+                            }
+            else {
+                var renta = calcRenta(money)
+                var isss = money * 0.03
+                var afp = money * 0.725
+                var seguro = 0.00
+                if (money <= 250) seguro = money * 0.02
+                rentaTex.text = "Renta: " + renta.toString()
+                issText.text = "Isss: " +isss.toString()
+                afpText.text = "Afp: " +afp.toString()
+                seguroText.text = "Seguro: " +seguro.toString()
+            }
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(DeduccionesViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    fun calcRenta(salario: Double): Double {
+        if (salario < 550) {
+            return 0.00
+        }
+        if (salario > 550.01 && salario < 800) {
+            return 0.1 * salario
+        }
+        if (salario > 800.01 && salario < 2500) {
+            return 0.2 * salario
+        }
+        if (salario > 2500.01) {
+            return 0.3 * salario
+        }
+        return 0.00
     }
 
 }
